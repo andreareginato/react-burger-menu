282fc73bc9747fee5f62096ac14b8f4142515f02
Andrea Reginato <andrea.reginato@gmail.com>
2019-09-24 11:21:31 +0200

    Keep the menu open

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -29,27 +29,27 @@ const styles = {
         ? ''
         : right
         ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
+        : 'translate3d(-85%, 0, 0)',
       MsTransform: isOpen
         ? ''
         : right
         ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
+        : 'translate3d(-85%, 0, 0)',
       OTransform: isOpen
         ? ''
         : right
         ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
+        : 'translate3d(-85%, 0, 0)',
       WebkitTransform: isOpen
         ? ''
         : right
         ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
+        : 'translate3d(-85%, 0, 0)',
       transform: isOpen
         ? ''
         : right
         ? 'translate3d(100%, 0, 0)'
-        : 'translate3d(-100%, 0, 0)',
+        : 'translate3d(-85%, 0, 0)',
       transition: 'all 0.5s'
     };
   },
